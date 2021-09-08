5f111876ff5aa276b26147d1ee52a79390fcbb99
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-09-09 01:34:02 +0900

    Update music.py

--- addons/music.py
+++ addons/music.py
@@ -9,10 +9,20 @@ from module.cursor import Cursor
 
 
 class Music(dico_command.Addon):
-    @staticmethod
-    def verify_voice_state(voice_state):
+    def verify_voice_state(self, voice_state: dico.VoiceState, *, check_connected: bool = False, check_playing: bool = False, check_paused: bool = False):
         if not voice_state or not voice_state.channel_id:
             return "Please connect or reconnect to the voice channel first."
+
+        player = self.bot.lava.player_manager.get(int(voice_state.guild_id))
+
+        if check_connected and player is None:
+            return 2, "Please play any music first."
+
+        if check_playing and player.paused:
+            return 3, "Player is paused. Please resume the player first."
+
+        if check_paused and not player.paused:
+            return 4, "Player is currently playing. Please pause the player first."
         return None
 
     @dico_command.command("connect")
@@ -21,8 +31,10 @@ class Music(dico_command.Addon):
         vv = self.verify_voice_state(voice_state)
         if vv:
             return await ctx.reply(embed=dico.Embed(description=vv, color=0xe74c3c))
+        msg = await ctx.reply(embed=dico.Embed(description="Please wait, connecting..."))
         self.bot.lava.player_manager.create(int(ctx.guild_id), region="ko")
         await self.bot.update_voice_state(ctx.guild_id, voice_state.channel_id)
+        await msg.edit(embed=dico.Embed(description="Successfully connected to voice channel!", color=0x3dd415))
 
     @dico_command.command("disconnect")
     async def disconnect(self, ctx: dico_command.Context):
@@ -50,7 +62,7 @@ class Music(dico_command.Addon):
         msg = await ctx.reply(embed=dico.Embed(description="Found the music, please wait..."))
         track = None
         if resp["loadType"] == "PLAYLIST_LOADED":
-            return await msg.edit(content="Sorru playlist is not supported.")
+            return await msg.edit(embed=dico.Embed(description="Sorry, playlist is not supported.", color=0xe74c3c))
         elif resp["loadType"] == "SEARCH_RESULT":
             await msg.delete()
             tracks = resp["tracks"]
@@ -67,21 +79,21 @@ class Music(dico_command.Addon):
             _msg, resp = cursor.result
             await _msg.delete()
             if resp is None:
-                return await ctx.reply(content="Canceled player.")
-            msg = await ctx.reply(content="Please wait, preparing...")
+                return await ctx.reply(embed=dico.Embed(description="Canceled player.", color=0xe74c3c))
+            msg = await ctx.reply(embed=dico.Embed(description="Please wait, preparing...", color=utils.rgb(225, 225, 225)))
             track = tracks[resp]
         track = track or resp['tracks'][0]
-        player.add(requester=ctx.author.id, track=track)
+        player.add(requester=int(ctx.author.id), track=track)
         if not player.is_playing:
             await player.play()
-            await msg.edit(content="", embed=dico.Embed(description=f"Playing [{track['info']['title']}]({track['info']['uri']}).", color=0x3dd415))
+            await msg.edit(embed=dico.Embed(description=f"Playing [{track['info']['title']}]({track['info']['uri']}).", color=0x3dd415))
         else:
-            await msg.edit(content="", embed=dico.Embed(description=f"Added [{track['info']['title']}]({track['info']['uri']}) to queue.", color=0x3dd415))
+            await msg.edit(embed=dico.Embed(description=f"Added [{track['info']['title']}]({track['info']['uri']}) to queue.", color=0x3dd415))
 
     @dico_command.command("skip")
     async def skip(self, ctx: dico_command.Context):
         voice_state = ctx.author.voice_state
-        vv = self.verify_voice_state(voice_state)
+        vv = self.verify_voice_state(voice_state, check_connected=True, check_playing=True)
         if vv:
             return await ctx.reply(embed=dico.Embed(description=vv, color=0xe74c3c))
         player = self.bot.lava.player_manager.get(int(ctx.guild_id))
@@ -92,27 +104,37 @@ class Music(dico_command.Addon):
     @dico_command.command("loop")
     async def loop(self, ctx: dico_command.Context):
         voice_state = ctx.author.voice_state
-        vv = self.verify_voice_state(voice_state)
+        vv = self.verify_voice_state(voice_state, check_connected=True)
         if vv:
             return await ctx.reply(embed=dico.Embed(description=vv, color=0xe74c3c))
         player = self.bot.lava.player_manager.get(int(ctx.guild_id))
         player.set_repeat(not player.repeat)
-        await ctx.reply(f"Repeat is now {'enabled' if player.repeat else 'disabled'}.")
+        await ctx.reply(embed=dico.Embed(description=f"Repeat is now {'enabled' if player.repeat else 'disabled'}.", color=0x3dd415))
+
+    @dico_command.command("shuffle")
+    async def shuffle(self, ctx: dico_command.Context):
+        voice_state = ctx.author.voice_state
+        vv = self.verify_voice_state(voice_state, check_connected=True)
+        if vv:
+            return await ctx.reply(embed=dico.Embed(description=vv, color=0xe74c3c))
+        player = self.bot.lava.player_manager.get(int(ctx.guild_id))
+        player.set_shuffle(not player.shuffle)
+        await ctx.reply(embed=dico.Embed(description=f"Shuffle is now {'enabled' if player.shuffle else 'disabled'}.", color=0x3dd415))
 
     @dico_command.command("volume")
     async def volume(self, ctx, vol: int = None):
         voice_state = ctx.author.voice_state
-        vv = self.verify_voice_state(voice_state)
+        vv = self.verify_voice_state(voice_state, check_connected=True, check_playing=True)
         if vv:
             return await ctx.reply(embed=dico.Embed(description=vv, color=0xe74c3c))
         player = self.bot.lava.player_manager.get(int(ctx.guild_id))
         if not vol:
-            return await ctx.reply(f"Current volume is `{player.volume}`%.")
+            return await ctx.reply(embed=dico.Embed(description=f"Current volume is `{player.volume}`%.", color=utils.rgb(225, 225, 225)))
         vol = int(vol)
         if not 0 < vol <= 1000:
             return await ctx.reply(embed=dico.Embed(description="Volume must be between 1 and 1000.", color=0xe74c3c))
         await player.set_volume(vol)
-        await ctx.reply(f"Volume is now set to `{vol}`%.")
+        await ctx.reply(embed=dico.Embed(description=f"Volume is now set to `{vol}`%.", color=0x3dd415))
 
 
 def load(bot: dico_command.Bot):
