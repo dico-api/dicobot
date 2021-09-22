03b775e4732e54caaa10d388fe2787c71ed967c4
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-09-22 13:19:44 +0900

    Use dp

--- main.py
+++ main.py
@@ -50,4 +50,5 @@ async def reload(ctx: dico_command.Context, name: str):
 
 
 [bot.load_module(f"addons.{x.replace('.py', '')}") for x in os.listdir("./addons") if x.endswith('.py') and not x.startswith("_")]
+bot.load_module("dp")
 bot.run()
